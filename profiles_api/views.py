5c2cbe07157dcad2cb777250f4a47bcb712e1c2d
Mehmet Ali Kalender <mehmetalikalender5@gmail.com>
2025-09-04 16:38:57 +0300

    Added user profile feed api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -9,7 +9,7 @@ from profiles_api import permissions
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-
+from rest_framework.permissions import IsAuthenticated
 
 class HelloApiView(APIView):
     """Test API view"""
@@ -83,7 +83,7 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({'http_method':'PUT'})
     def partial_update(self,request,pk = None):
         """Handle updating part od an object"""
-        return Reponse({'http_method': 'PATCH'})
+        return Response({'http_method': 'PATCH'})
     def destroy(self,request,pk = None):
         """Handle removing an object"""
         return Response({'http_method':'DELETE'})
@@ -104,5 +104,15 @@ class UserLoginApiView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
     
 
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating,reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus,IsAuthenticated)
+    def perform_create(self,serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile = self.request.user)
+        
 
 
